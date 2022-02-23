bb8e8087ea3d27320dce52e0168d3d57e945cc97
yogender77 <yogender.koppu@thundersoft.com>
2022-02-23 22:54:17 +0530

    3d array using pointer

---
+++ pointer3darray.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int arr[2][3][2] = {
+					{
+						{5, 10},
+						{6, 11},
+						{7, 12},
+					},
+					{
+						{20, 30},
+						{21, 31},
+						{22, 32},
+					}
+					};
+int i, j, k;
+for (i = 0; i < 2; i++)
+{
+	for (j = 0; j < 3; j++)
+	{
+	for (k = 0; k < 2; k++)
+		printf("%d\t", *(*(*(arr + i) + j) +k));
+	printf("\n");
+	}
+}
+
+return 0;
+}

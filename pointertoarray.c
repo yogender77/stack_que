3f4304088bc4731500451a5d9acd8296c3e41b92
yogender77 <yogender.koppu@thundersoft.com>
2022-02-23 22:49:33 +0530

    pointer to array

---
+++ pointertoarray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+	// Pointer to an integer
+	int *p;
+	
+	// Pointer to an array of 5 integers
+	int (*ptr)[5];
+	int arr[5];
+	
+	// Points to 0th element of the arr.
+	p = arr;
+	
+	// Points to the whole array arr.
+	ptr = &arr;
+	
+	printf("p = %p, ptr = %p\n", p, ptr);
+	
+	p++;
+	ptr++;
+	
+	printf("p = %p, ptr = %p\n", p, ptr);
+	
+	return 0;
+}

31e603e41914c268f89b151c1db4d190cfe59be5
yogender77 <yogender.koppu@thundersoft.com>
2022-03-01 17:47:24 +0530

    pattern1

---
+++ pattern1.c
@@ -0,0 +1,23 @@
+/*
+*****
+*****
+*****
+*****
+*****
+*/
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n=5;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)//*****
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

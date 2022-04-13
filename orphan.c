251d9f73b4a0d54c07484452dcd746fb5b7addba
yogender77 <yogender.koppu@thundersoft.com>
2022-04-13 20:04:01 +0530

    orphan

---
+++ orphan.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int main()
+{
+pid_t p;
+p=fork();
+if(p==0)                                   //child
+{
+printf("I am child having PID %d\n",getpid());
+printf("My parent PID is %d\n",getppid());
+}
+else                                          //parent
+{
+	sleep(2);
+printf("I am Parent PID %d\n",getpid());
+printf("MY child PID is %d\n",p);
+}
+}
